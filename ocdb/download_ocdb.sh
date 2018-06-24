3c35af105bd787a15a72c6fe87364543e8185972
Laurent Aphecetche <laurent.aphecetche@gmail.com>
2018-06-24 20:08:20 +0200

    Split the generation of file list to be downloaded and downloading itself

--- ocdb/download_ocdb.sh
+++ ocdb/download_ocdb.sh
@@ -3,23 +3,39 @@
 # script to download all the OCDB files of a given year
 # for some defined types.
 # The intent being to have a local copy of all the files
-# required to do a scan of the OCDB, e.g. to compute 
+# required to do a scan of the OCDB, e.g. to compute
 # the integrated luminosity, or assess the HV issues
 # of the year, etc...
 
-year=2017
+year=${1:-2018}
 
-#for type in GRP/CTP/Config GRP/GRP/Data GRP/CTP/Scalers GRP/GRP/LHCData 
-for type in MUON/Calib/Pedestals MUON/Calib/Config MUON/Calib/HV MUON/Calib/LV MUON/Calib/BPEVO MUON/Calib/OccupancyMap
+grp="GRP/CTP/Config GRP/GRP/Data GRP/CTP/Scalers GRP/GRP/LHCData"
+
+muon="MUON/Calib/MappingData MUON/Calib/MappingRunData MUON/Calib/RecoParam MUON/Calib/Pedestals MUON/Calib/Config MUON/Calib/HV MUON/Calib/LV MUON/Calib/BPEVO MUON/Calib/OccupancyMap"
+
+match="/"
+repl="_"
+
+# generate the lists first
+for t in $(echo $grp $muon)
 do
-    path=/alice/data/$year/OCDB/$type
-    list=${path////_}.txt
-    echo "Making file list for $type -> $list"
-    alien_find $path "*.root" | grep alice | sed s_/alice_alien:///alice_g >  $list
-    echo "Downloading files for $type"
+    bpath=/alice/data/$year/OCDB/$t
+    list=${bpath//$match/$repl}.txt
+    if test -e $list; then
+        echo "File list $list already exist. Not replacing it. Do it by hand if need be."
+    else
+        echo "Making file list for $t -> $list"
+        alien_find $bpath "*.root" | grep alice | sed s_/alice_alien:///alice_g >  $list
+    fi
+done
+
+# now loop over the lists to download files
+for t in $(ls _alice_data_${year}_*.txt)
+do
+    echo "Downloading files for $t"
     root -b <<EOF
 gSystem->Load("$HOME/github.com/aphecetche/aafu/libmyaf.so");
-VAF::CopyFromRemote("$list");
+VAF::CopyFromRemote("$t");
 .q
 EOF
 done
